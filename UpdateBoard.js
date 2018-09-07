a8b6ed0539930526271920eb753d787a310553f0
Suzanne Huldt <suzannegchuldt@gmail.com>
2018-09-07 20:44:43 +0100

    first commit

---
+++ UpdateBoard.js
@@ -0,0 +1,23 @@
+class UpdateBoard{
+
+  updateBoard(board){
+
+    if(this.isDead(cell) && //){
+      else if(||//){
+
+      }
+    }
+  }
+
+  isDead(cell){
+    return cell == 0
+  }
+
+  grabEnvironment(cell, board){
+    var cX = cell[0];
+    var cY = cell[1];
+    var environment = [];
+    environment.push()
+  }
+
+}
